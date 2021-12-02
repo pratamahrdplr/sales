97b184a21b1afb967b16b1c6f9d401b35e2ad16b
yanto <mrthx.89@gmail.com>
2021-12-02 21:37:48 +0700

    data usul tambah propinsi dan kota

--- src/sales/input_usul.php
+++ src/sales/input_usul.php
@@ -1,77 +1,112 @@
 <link rel="stylesheet" href="js/jquery-ui.css">
-  <link rel="stylesheet" href="css/style.css">
-    <script src="js/jquery-1.6.4.js"></script>
-  <script src="js/jquery-ui.js"></script>
+<link rel="stylesheet" href="css/style.css">
+<script src="js/jquery-1.6.4.js"></script>
+<script src="js/jquery-ui.js"></script>
 
-  <?php
-   $user_query = mysql_query("select nama_kontak from tb_kontak_all ")or die(mysql_error());
-                    while($row_user = mysql_fetch_array($user_query, MYSQL_ASSOC)){
+<?php
+$user_query = mysql_query("select nama_kontak from tb_kontak_all ") or die(mysql_error());
+while ($row_user = mysql_fetch_array($user_query, MYSQL_ASSOC)) {
 
-					
-					$output[] = $row_user['nama_kontak'];
-			
-					}
-					
-					
-					
-?>					
+
+  $output[] = $row_user['nama_kontak'];
+}
+
+
+
+?>
 
 <script>
-var data = <?php echo json_encode($output); ?>;
-  $( function() {
-    $( "#nama" ).autocomplete({
+  var data = <?php echo json_encode($output); ?>;
+  $(function() {
+    $("#nama").autocomplete({
       source: data
     });
-  } );
-  </script>
+  });
+</script>
 </head>
+
 <body>
   <?php
-						$session_user = $_SESSION['user'];
-
-	
-		?>  
-<div class="ui-widget">
-   <form role="form" action="insert_usul.php" enctype="multipart/form-data" method="post">
-                  <input name="id_usr" type="hidden" value="<?php echo $session_user; ?>" />
-                                   <input name="user" type="hidden" value="<?php echo $user_username; ?>" />
-
-                    <!-- text input -->
-                                        <div class="form-group">
-                      <label>Nama Perusahaan </label>
-                      <input type="text" class="form-control" placeholder="Masukan Nama..." name="nama" value="" required>
-                    </div>
-                    <div class="form-group">
-                      <label>Alamat</label>
-                      <input type="text" class="form-control" placeholder="Masukan Telp..." name="alamat" value="" required>
-                    </div>
-					 <div class="form-group">
-                      <label>Kota</label>
-                      <input type="text" class="form-control" placeholder="Masukan Kota..." name="kota" value="" required>
-                    </div>
-					<div class="form-group">
-                      <label>Nama Purchasing</label>
-                      <input type="text" class="form-control" placeholder="Masukan Nama Purchasing ..." name="cp" value="" required>
-                    </div>
-                    <div class="form-group">
-                      <label>Telp. Perusahaan</label>
-                      <input type="text" class="form-control" placeholder="Masukan Telp..." name="telepon" value="" required>
-                    </div>
-                     <div class="form-group">
-                      <label>Fax</label>
-                      <input type="text" class="form-control" placeholder="Masukan Fax ..." name="fax" value="" >
-                    </div>
-                    <div class="form-group">
-                      <label for="email">Email</label>
-                      <input type="email" class="form-control " placeholder="Masukan Email ..." name="email" value="" >
-                    </div>
-                    
-                   
-                  
-<div class="panel-footer">
-                    <button type="submit" class="btn btn-success">Usulkan</button>
-                  </div>
+  $session_user = $_SESSION['user'];
 
 
-                  </form>
+  ?>
+  <div class="ui-widget">
+    <form role="form" action="insert_usul.php" enctype="multipart/form-data" method="post">
+      <input name="id_usr" type="hidden" value="<?php echo $session_user; ?>" />
+      <input name="user" type="hidden" value="<?php echo $user_username; ?>" />
+
+      <!-- text input -->
+      <div class="form-group">
+        <label>Nama Perusahaan </label>
+        <input type="text" class="form-control" placeholder="Masukan Nama..." name="nama" value="" required>
+      </div>
+      <div class="form-group">
+        <label>Alamat</label>
+        <input type="text" class="form-control" placeholder="Masukan Alamat..." name="alamat" value="" required>
+      </div>
+      <div class="form-group">
+        <label>Provinsi</label>
+        <select id="provinsi" name="provinsi">
+          <option value="">Pilih Provinsi</option>
+          <?php
+          $query = mysql_query("SELECT * FROM provinsi ORDER BY nama_provinsi");
+          while ($row = mysql_fetch_array($query)) {
+            ?>
+            <option value="<?php echo $row['id_provinsi']; ?>">
+              <?php echo $row['nama_provinsi']; ?>
+            </option>
+          <?php
+          }
+          ?>
+        </select>
+      </div>
+      <div class="form-group">
+        <label>Kota</label>
+        <select id="kota" name="kota">
+          <option value="">Pilih Kota</option>
+          <?php
+          $query = mysql_query("SELECT * FROM kota INNER JOIN provinsi ON kota.id_provinsi = provinsi.id_provinsi ORDER BY nama_kota");
+          while ($row = mysql_fetch_array($query)) {
+            ?>
+            <option id="kota" class="<?php echo $row['id_provinsi']; ?>" value="<?php echo $row['id_kota']; ?>">
+              <?php echo $row['nama_kota']; ?>
+            </option>
+          <?php
+          }
+          ?>
+        </select>
+      </div>
+
+      <div class="form-group">
+        <label>Nama Purchasing</label>
+        <input type="text" class="form-control" placeholder="Masukan Nama Purchasing ..." name="cp" value="" required>
+      </div>
+      <div class="form-group">
+        <label>Telp. Perusahaan</label>
+        <input type="text" class="form-control" placeholder="Masukan Telp..." name="telepon" value="" required>
+      </div>
+      <div class="form-group">
+        <label>Fax</label>
+        <input type="text" class="form-control" placeholder="Masukan Fax ..." name="fax" value="">
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control " placeholder="Masukan Email ..." name="email" value="">
+      </div>
+
+
+
+      <div class="panel-footer">
+        <button type="submit" class="btn btn-success">Usulkan</button>
+      </div>
+
+
+    </form>
+    <script src="jquery-1.10.2.min.js"></script>
+    <script src="jquery.chained.min.js"></script>
+    <script>
+      $("#kota").chained("#provinsi");
+      $("#kecamatan").chained("#kota");
+    </script>
   </div>
--- src/sales/insert_usul.php
+++ src/sales/insert_usul.php
@@ -1,13 +1,17 @@
-
 <?php
- include('../dbcon.php');
-dbcon(); 
+include('../dbcon.php');
+dbcon();
 
-$query=mysql_query("insert into tb_kontak_usul(id_user, nama_kontak, alamat_kontak, kota_kontak, cp_kontak, telepon_kontak, fax_kontak, email_kontak, status_kontak, pembuat) values ('$_POST[id_usr]', '$_POST[nama]','$_POST[alamat]','$_POST[kota]','$_POST[cp]','$_POST[telepon]','$_POST[fax]','$_POST[email]', 'menunggu', '$_POST[user]')") or die (mysql_error());
+$queryp = mysql_query("SELECT * FROM provinsi WHERE id_provinsi = '$_POST[provinsi]'");
+$rowp = mysql_fetch_array($queryp);
+$provinsi = $rowp['nama_provinsi'];
 
-if($query){
-header("location:index.php?page=data_usul");
+$queryk = mysql_query("SELECT * FROM kota WHERE id_kota = '$_POST[kota]'");
+$rowk = mysql_fetch_array($queryk);
+$kota = $rowk['nama_kota'];
 
+$query = mysql_query("insert into tb_kontak_usul(id_user, nama_kontak, alamat_kontak, kota_kontak, provinsi_kontak, cp_kontak, telepon_kontak, fax_kontak, email_kontak, status_kontak, tanggal_kontak, pembuat) values ('$_POST[id_usr]', '$_POST[nama]','$_POST[alamat]','$kota','$provinsi','$_POST[cp]','$_POST[telepon]','$_POST[fax]','$_POST[email]', 'menunggu', NOW(), '$_POST[user]')") or die(mysql_error());
+
+if ($query) {
+    header("location:index.php?page=data_usul");
 }
-
-?>
